4219984b8e8441273be720ff04c2fa2c1aa20224
muhammad-mustafiz-billah <mustafizbillah333@gmail.com>
2026-09-17 19:48:07 +0600

    feat: add LIPI banner and personalized user greetings

--- main.go
+++ main.go
@@ -7,15 +7,6 @@ import (
 	"os/user"
 )
 
-// func main() {
-// 	user, err := user.Current()
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// 	fmt.Printf("Hello %s! This is the Lipi programming language!\n", user.Username)
-// 	fmt.Printf("Feel free to type in commands\n")
-// 	repl.Start(os.Stdin, os.Stdout)
-// }
 func main() {
     currentUser, err := user.Current()
     if err != nil {
