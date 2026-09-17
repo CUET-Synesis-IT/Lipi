4181b5870ad8e23d6e8bde78df07f5347e2d994e
muhammad-mustafiz-billah <mustafizbillah333@gmail.com>
2026-09-17 20:11:17 +0600

    feat: add LIPI banner and personalized user greetings

--- main.go
+++ main.go
@@ -8,11 +8,23 @@ import (
 )
 
 func main() {
-	user, err := user.Current()
-	if err != nil {
-		panic(err)
-	}
-	fmt.Printf("Hello %s! This is the Lipi programming language!\n", user.Username)
-	fmt.Printf("Feel free to type in commands\n")
-	repl.Start(os.Stdin, os.Stdout)
+    currentUser, err := user.Current()
+    if err != nil {
+        panic(err)
+    }
+
+    fmt.Println(`
+██╗     ██╗██████╗ ██╗
+██║     ██║██╔══██╗██║
+██║     ██║██████╔╝██║
+██║     ██║██╔═══╝ ██║
+███████╗██║██║     ██║
+╚══════╝╚═╝╚═╝     ╚═╝
+`)
+
+    fmt.Printf("Hello %s! This is the Lipi programming language!\n", currentUser.Username)
+    fmt.Println("Type 'exit' to quit.")
+    fmt.Println()
+
+    repl.Start(os.Stdin, os.Stdout, currentUser.Username)
 }
