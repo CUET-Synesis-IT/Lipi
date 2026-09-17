5d34a40ef7172b3c2305dd56bf9ef111a7caaf9f
Zarif Muhtasim <roundspecs@pm.me>
2026-09-17 20:14:11 +0600

    chore: format code

--- main.go
+++ main.go
@@ -8,23 +8,22 @@ import (
 )
 
 func main() {
-    currentUser, err := user.Current()
-    if err != nil {
-        panic(err)
-    }
+	currentUser, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
 
-    fmt.Println(`
+	fmt.Println(`
 ██╗     ██╗██████╗ ██╗
 ██║     ██║██╔══██╗██║
 ██║     ██║██████╔╝██║
 ██║     ██║██╔═══╝ ██║
 ███████╗██║██║     ██║
-╚══════╝╚═╝╚═╝     ╚═╝
-`)
+╚══════╝╚═╝╚═╝     ╚═╝`)
 
-    fmt.Printf("Hello %s! This is the Lipi programming language!\n", currentUser.Username)
-    fmt.Println("Type 'exit' to quit.")
-    fmt.Println()
+	fmt.Printf("Hello %s! This is the Lipi programming language!\n", currentUser.Username)
+	fmt.Println("Type 'exit' to quit.")
+	fmt.Println()
 
-    repl.Start(os.Stdin, os.Stdout, currentUser.Username)
+	repl.Start(os.Stdin, os.Stdout, currentUser.Username)
 }
