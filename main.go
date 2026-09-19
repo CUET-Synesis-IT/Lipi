7e4b1c3f285cce081c6cdc108019c39e1fecb61e
muhammad-mustafiz-billah <mustafizbillah333@gmail.com>
2026-09-19 19:58:37 +0600

    feat: add command-line interface for Lipi

--- main.go
+++ main.go
@@ -2,28 +2,57 @@ package main
 
 import (
 	"fmt"
-	"lipi/repl"
 	"os"
 	"os/user"
+	"path/filepath"
+	"lipi/repl"
+	"lipi/runner"
 )
 
 func main() {
-	currentUser, err := user.Current()
-	if err != nil {
-		panic(err)
-	}
+	args := os.Args
 
-	fmt.Println(`
+	if len(args) == 1 {
+		currentUser, err := user.Current()
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println(`
 ██╗     ██╗██████╗ ██╗
 ██║     ██║██╔══██╗██║
 ██║     ██║██████╔╝██║
 ██║     ██║██╔═══╝ ██║
 ███████╗██║██║     ██║
-╚══════╝╚═╝╚═╝     ╚═╝`)
+╚══════╝╚═╝╚═╝     ╚═╝
+`)
 
-	fmt.Printf("Hello %s! This is the Lipi programming language!\n", currentUser.Username)
-	fmt.Println("Type 'exit' to quit.")
-	fmt.Println()
+		fmt.Printf("Hello %s! This is the Lipi programming language!\n", currentUser.Username)
+		fmt.Println("Type 'exit' to quit.")
+		fmt.Println()
 
-	repl.Start(os.Stdin, os.Stdout, currentUser.Username)
+		repl.Start(os.Stdin, os.Stdout, currentUser.Username)
+		return
+	}
+
+	if len(args) == 2 {
+		filename := args[1]
+
+		if filepath.Ext(filename) != ".lipi" {
+			fmt.Fprintln(os.Stderr, "Error: file must have .lipi extension")
+			os.Exit(1)
+		}
+
+		if err := runner.RunFile(filename); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+
+		return
+	}
+
+	fmt.Fprintln(os.Stderr, "Usage:")
+	fmt.Fprintln(os.Stderr, "  lipi")
+	fmt.Fprintln(os.Stderr, "  lipi file.lipi")
+	os.Exit(1)
 }
